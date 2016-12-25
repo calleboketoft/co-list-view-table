bd83f4ea796039e8cb73e73cef0bf253eb53e5fb
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-12-25 09:45:33 +0900

    build

--- src/ng2-table/ng2-table.component.js
+++ src/ng2-table/ng2-table.component.js
@@ -17,6 +17,8 @@ var Ng2TableComponent = (function () {
         this.tableConfigUpdated = new core_1.EventEmitter();
         this.rowClickStyles = false;
     }
+    // Deep copy the parts of the tableConfig that will be modified when
+    // sorting and searching the columns
     Ng2TableComponent.prototype.copyTableConfig = function (tableConfig) {
         // columnDefs need to be deep copied
         var columnDefsCopy = tableConfig.columnDefs.map(function (colDef) {
@@ -35,7 +37,7 @@ var Ng2TableComponent = (function () {
         return tableConfigCopy;
     };
     Ng2TableComponent.prototype.ngOnChanges = function (changes) {
-        // add search terms etc to this one
+        // add search terms and sorting to a copy of tableConfig
         // the only problem would be if we want to send in a new tableConfig
         // via the @Input() since we're now working with a copy
         this.tableConfigCopy = this.tableConfigCopy || this.copyTableConfig(this.tableConfig);
@@ -47,7 +49,7 @@ var Ng2TableComponent = (function () {
             var sortAdvanced = this.tableConfigCopy.columnDefs.find(function (colDef) {
                 return !!colDef.sortAdvanced;
             });
-            // sortAdvanced takes precedence in sorting
+            // sortAdvanced takes precedence over sortDefault in sorting
             if (sortAdvanced) {
                 // there might be multiple columns with advanced sort
                 this.sortColsAdvanced(this.tableConfigCopy.columnDefs);
@@ -58,10 +60,10 @@ var Ng2TableComponent = (function () {
                 });
                 if (basicSortColIndex !== -1) {
                     var colToSort = this.tableConfigCopy.columnDefs[basicSortColIndex];
+                    // convert sortDefault to sortAdvanced to be able to simplify further
+                    // column sorting
                     colToSort.sortAdvanced = {
                         count: 1,
-                        // convert the basic sort to the opposite sorting direction of the
-                        // default sort since the colHeaderSortClicked reverses the direction by default
                         direction: colToSort.sortDefault ? 1 : -1
                     };
                     this.tableData = sorter_service_1.tableDataSort(colToSort.field, this.tableData, colToSort.sortAdvanced.direction);
@@ -69,22 +71,24 @@ var Ng2TableComponent = (function () {
             }
         }
     };
-    Ng2TableComponent.prototype.selectRow = function (dataRow, rowIndex) {
+    Ng2TableComponent.prototype.rowClickedFn = function (dataRow, rowIndex) {
         this.activeRow = rowIndex;
         this.rowClicked.emit(dataRow);
     };
-    // Custom button clicked
-    Ng2TableComponent.prototype.buttonFn = function ($event, colSpec, row) {
-        $event.stopPropagation(); // don't want to trigger "selectRow" as well
+    Ng2TableComponent.prototype.cellItemClickedFn = function ($event, colSpec, row) {
+        $event.stopPropagation(); // don't want to trigger "rowClickedFn" as well
         this.cellItemClicked.emit({ colSpec: colSpec, row: row });
     };
-    Ng2TableComponent.prototype.searchUpdate = function ($event) {
+    Ng2TableComponent.prototype.searchUpdate = function (originalCol) {
+        // The incoming col is from the original tableConfig, which we don't want
+        // to modify. Find the col from the tableConfigCopy and modify that one instead
         var foundColDef = this.tableConfigCopy.columnDefs.find(function (colDef) {
-            return colDef.field === $event.field;
+            return colDef.field === originalCol.field;
         });
-        // Add search term to the colDef for the field being searched
-        foundColDef.searchTerm = $event.value;
-        // NOTE not 100% sure why I have to do this
+        // add search term to the colDef for the field being searched
+        foundColDef.searchTerm = originalCol.value;
+        // NOTE not 100% sure why I have to do this, foundColDef should've been
+        // a reference to the tableConfigCopy item?
         var updatedTableConfigCopy = this.copyTableConfig(this.tableConfigCopy);
         this.tableConfigCopy = updatedTableConfigCopy;
         this.tableConfigUpdated.emit(updatedTableConfigCopy);
@@ -95,6 +99,7 @@ var Ng2TableComponent = (function () {
         var columnsToApplySorting = columnDefs.filter(function (colDef) {
             return !!colDef.sortAdvanced;
         });
+        // use the count property to figure out in which order the columns were sorted
         columnsToApplySorting.sort(function (a, b) {
             if (a.sortAdvanced.count < b.sortAdvanced.count) {
                 return -1;
@@ -102,7 +107,6 @@ var Ng2TableComponent = (function () {
             if (a.sortAdvanced.count > b.sortAdvanced.count) {
                 return 1;
             }
-            // a must be equal to b
             return 0;
         });
         // sort all the columns with sortAdvanced, in order
@@ -111,14 +115,14 @@ var Ng2TableComponent = (function () {
             return mem;
         }, this.tableData);
     };
-    Ng2TableComponent.prototype.colHeaderSortClicked = function (col) {
+    Ng2TableComponent.prototype.colHeaderSortClicked = function (originalCol) {
         // The incoming col is from the original tableConfig, which we don't want
         // to modify. Find the col from the tableConfigCopy and modify that one instead
         var colInCopy = this.tableConfigCopy.columnDefs.find(function (colDef) {
-            return col.field === colDef.field;
+            return originalCol.field === colDef.field;
         });
         // Find the current highest sort count. Every time a column header is clicked
-        // for sorting, the counter gets increased. This can be used to create an
+        // for sorting, the counter gets incremented. This can be used to recreate an
         // exact sort order based on multiple columns being sorted.
         var maxSortCount = this.tableConfigCopy.columnDefs.reduce(function (mem, curr) {
             if (curr.sortAdvanced && curr.sortAdvanced.count > mem) {
@@ -130,8 +134,10 @@ var Ng2TableComponent = (function () {
         // Set the sort count to max + 1, this is the most recently pressed sort
         colInCopy.sortAdvanced.count = maxSortCount + 1;
         // if the column sort has been pressed already, switch direction
-        colInCopy.sortAdvanced.direction = colInCopy.sortAdvanced.direction ? -colInCopy.sortAdvanced.direction : 1;
-        // Update columnDef for sorted item
+        colInCopy.sortAdvanced.direction = colInCopy.sortAdvanced.direction
+            ? -colInCopy.sortAdvanced.direction
+            : 1;
+        // Update columnDef for sorted item in the tableConfigCopy
         var colDefIndexToReplace = this.tableConfigCopy.columnDefs.findIndex(function (colDef) {
             return colDef.field === colInCopy.field;
         });
@@ -217,7 +223,7 @@ Ng2TableComponent = __decorate([
     core_1.Component({
         selector: 'ng2-table',
         styles: ["\n    /**\n     * Scrollable tbody\n     * https://jsfiddle.net/tsayen/xuvsncr2/28/\n     * http://stackoverflow.com/questions/17067294/html-table-with-100-width-with-vertical-scroll-inside-tbody\n     */\n\n    table {\n      display: flex;\n      flex-flow: column;\n      height: 100%;\n      width: 100%;\n    }\n    table thead {\n      /* head takes the height it requires,\n      and it's not scaled when table is resized */\n      flex: 0 0 auto;\n      width: calc(100% - 0.9em);\n    }\n    table tbody {\n      /* body takes all the remaining available space */\n      flex: 1 1 auto;\n      display: block;\n      overflow: auto;\n    }\n    table tbody tr {\n      width: 100%;\n    }\n    table thead, table tbody tr {\n      display: table;\n      table-layout: fixed;\n    }\n\n    /**\n     * Appearance\n     */\n\n    th span:hover {\n      cursor: pointer;\n    }\n    .table thead th {\n      vertical-align: top;\n      border-bottom: 1px solid #eceeef;\n    }\n    .search-wrap {\n      margin-top: 8px;\n    }\n\n    /**\n     * Preserve newlines\n     * http://stackoverflow.com/questions/10937218/how-to-show-multiline-text-in-a-table-cell\n     *\n     * And let word-wrap still work\n     * http://stackoverflow.com/a/4413129\n     */\n    td > div > .cell-content {\n      white-space: pre-wrap;\n    }\n  "],
-        template: "\n    <table\n      [ngClass]=\"getNgThing('table', 'class', tableConfig)\"\n      [ngStyle]=\"getNgThing('table', 'style', tableConfig)\">\n      <thead>\n        <tr>\n          <!-- must render from tableConfig so the table don't re-render all the\n               time when tableConfigCopy is updated -->\n          <th *ngFor=\"let col of tableConfig.columnDefs\"\n            [style.width]=\"col.width\"\n            [ngClass]=\"getNgThing('header', 'class', tableConfig, null, null, null, col)\"\n            [ngStyle]=\"getNgThing('header', 'style', tableConfig, null, null, null, col)\">\n            <span (click)=\"colHeaderSortClicked(col)\">\n              {{col.headerText || col.field}}\n            </span>\n            <div *ngIf=\"isAnyFieldSearchable\" class=\"search-wrap\">\n              <search-input-cmp\n                *ngIf=\"col.search\"\n                [field]=\"col.field\"\n                (search)=\"searchUpdate($event)\">\n              </search-input-cmp>\n            </div>\n          </th>\n        </tr>\n      </thead>\n      <tbody>\n        <tr\n          *ngFor=\"let rowData of tableData | search: tableConfigCopy; let rowIndex = index; let activeRowz = activeRow\"\n          [ngClass]=\"getNgThing('row', 'class', tableConfig, rowData, rowIndex, activeRow)\"\n          [ngStyle]=\"getNgThing('row', 'style', tableConfig, rowData, rowIndex, activeRow)\"\n          (click)=\"selectRow(rowData, rowIndex)\">\n          <td *ngFor=\"let col of tableConfig.columnDefs\" [style.width]=\"col.width\">\n            <div [ngSwitch]=\"col.cellItem?.elementType\">\n\n              <!-- BUTTON -->\n              <div *ngSwitchCase=\"'button'\">\n                <div\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\">\n                  <button type=\"button\"\n                    [ngClass]=\"getNgThing('cellItemButton', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    [ngStyle]=\"getNgThing('cellItemButton', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    (click)=\"buttonFn($event, col, rowData)\"\n                    >{{col.cellItem?.staticContent || rowData[col.field]}}</button>\n                </div>\n              </div>\n\n              <!-- DIV -->\n              <div *ngSwitchCase=\"'div'\">\n                <div\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\">\n                  <div\n                    [ngClass]=\"getNgThing('cellItemDiv', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    [ngStyle]=\"getNgThing('cellItemDiv', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    (click)=\"buttonFn($event, col, rowData)\"\n                    >{{col.cellItem?.staticContent || rowData[col.field]}}</div>\n                </div>\n              </div>\n\n              <!-- NO ITEM -->\n              <div *ngSwitchDefault class=\"cell-content\"\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                >{{rowData[col.field]}}</div>\n            </div>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  "
+        template: "\n    <table\n      [ngClass]=\"getNgThing('table', 'class', tableConfig)\"\n      [ngStyle]=\"getNgThing('table', 'style', tableConfig)\">\n      <thead>\n        <tr>\n          <!-- must render from tableConfig so the table don't re-render all the\n               time when tableConfigCopy is updated -->\n          <th *ngFor=\"let col of tableConfig.columnDefs\"\n            [style.width]=\"col.width\"\n            [ngClass]=\"getNgThing('header', 'class', tableConfig, null, null, null, col)\"\n            [ngStyle]=\"getNgThing('header', 'style', tableConfig, null, null, null, col)\">\n            <span (click)=\"colHeaderSortClicked(col)\">\n              {{col.headerText || col.field}}\n            </span>\n            <div *ngIf=\"isAnyFieldSearchable\" class=\"search-wrap\">\n              <search-input-cmp\n                *ngIf=\"col.search\"\n                [field]=\"col.field\"\n                (search)=\"searchUpdate($event)\">\n              </search-input-cmp>\n            </div>\n          </th>\n        </tr>\n      </thead>\n      <tbody>\n        <tr\n          *ngFor=\"let rowData of tableData | search: tableConfigCopy; let rowIndex = index; let activeRowz = activeRow\"\n          [ngClass]=\"getNgThing('row', 'class', tableConfig, rowData, rowIndex, activeRow)\"\n          [ngStyle]=\"getNgThing('row', 'style', tableConfig, rowData, rowIndex, activeRow)\"\n          (click)=\"rowClickedFn(rowData, rowIndex)\">\n          <td *ngFor=\"let col of tableConfig.columnDefs\" [style.width]=\"col.width\">\n            <div [ngSwitch]=\"col.cellItem?.elementType\">\n\n              <!-- BUTTON -->\n              <div *ngSwitchCase=\"'button'\">\n                <div\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\">\n                  <button type=\"button\"\n                    [ngClass]=\"getNgThing('cellItemButton', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    [ngStyle]=\"getNgThing('cellItemButton', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    (click)=\"cellItemClickedFn($event, col, rowData)\"\n                    >{{col.cellItem?.staticContent || rowData[col.field]}}</button>\n                </div>\n              </div>\n\n              <!-- DIV -->\n              <div *ngSwitchCase=\"'div'\">\n                <div\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\">\n                  <div\n                    [ngClass]=\"getNgThing('cellItemDiv', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    [ngStyle]=\"getNgThing('cellItemDiv', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                    (click)=\"cellItemClickedFn($event, col, rowData)\"\n                    >{{col.cellItem?.staticContent || rowData[col.field]}}</div>\n                </div>\n              </div>\n\n              <!-- NO ITEM -->\n              <div *ngSwitchDefault class=\"cell-content\"\n                  [ngClass]=\"getNgThing('cell', 'class', tableConfig, rowData, rowIndex, activeRow, col)\"\n                  [ngStyle]=\"getNgThing('cell', 'style', tableConfig, rowData, rowIndex, activeRow, col)\"\n                >{{rowData[col.field]}}</div>\n            </div>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  "
     }),
     __metadata("design:paramtypes", [])
 ], Ng2TableComponent);
