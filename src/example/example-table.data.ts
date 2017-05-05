a8a1649ea85c88e24294157cf59ed213a3faeb02
Paulo José Pires Soares <pso@unwire.dk>
2017-05-05 16:01:37 +0200

    bugfix: sort numeric rows by its numeric value instead of by the string

--- src/example/example-table.data.ts
+++ src/example/example-table.data.ts
@@ -1,30 +1,30 @@
 let exampleData = [
   {
-    userId: '1',
+    userId: 1,
     userName: 'Carl \nOla',
     nickName: 'A',
     pet: 'dog'
   },
   {
-    userId: '2',
+    userId: 2,
     userName: 'Boke',
     nickName: 'B',
     pet: 'cat'
   },
   {
-    userId: '4',
+    userId: 12,
     userName: 'Pelle',
     nickName: 'A',
     pet: 'dragon'
   },
   {
-    userId: '5',
+    userId: 5,
     userName: 'Oskar',
     nickName: 'A',
     pet: 'smurf'
   },
   {
-    userId: '3',
+    userId: 3,
     userName: 'Hanui',
     nickName: 'A',
     pet: 'unicorn'
--- src/ng2-table/sorter.service.ts
+++ src/ng2-table/sorter.service.ts
@@ -1,17 +1,31 @@
+
+function sort<T> (a: T, b: T): number {
+  if (a > b) {
+    return 1
+  }
+  if (a < b) {
+    return -1
+  }
+
+  return 0
+}
+
 // https://github.com/thelgevold/angular-2-samples
 export function tableDataSort (key: string, data: any[], direction: number) {
   let dataCopy = data.map(i => Object.assign({}, i))
 
   dataCopy.sort((a, b) => {
-    let aTmp = (a[key] + '').toLowerCase()
-    let bTmp = (b[key] + '').toLowerCase()
-    if (aTmp === bTmp || direction === 0) {
-      return 0
-    } else if (aTmp > bTmp) {
-      return direction
+    let sortResult: number
+    let aTmp = a[key]
+    let bTmp = b[key]
+     
+    if (typeof aTmp === 'number' && typeof bTmp === 'number') {
+      sortResult = sort(aTmp, bTmp)
     } else {
-      return -direction
+      sortResult = sort((aTmp + '').toLowerCase(), (bTmp + '').toLowerCase())
     }
+
+    return sortResult * direction
   })
 
   return dataCopy
